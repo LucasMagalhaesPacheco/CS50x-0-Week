ad1336959f3075835529ca6c036f7bec07151e55
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2023-10-09 13:32:14 -0300

    Binary correct exercise

--- binary.c
+++ binary.c
@@ -20,19 +20,36 @@ int main(void)
         // Convertemos então utilizando a tabela ASCII o código em um número.
         int decimal = sentence[i];
 
-        // O For em questão vai fazer a seguinte configuração, se o numeros for menos que Bits em um Byte, ele vai continuar no loop
-        //O que isso quer dizer que se o número for menor que 8 ele vai continuar, por sua vez
-        // Como queremos exatos 8 digitos, podemos fazer assim pois a contagem do for começa com 0.
+        //Definimos uma variável para ter os bits corretos dentro do array ou seja um array de 8;
+        int bits[BITS_IN_BYTE];
 
+
+        //Temos que lembrar que o indice sempre é zero
+        // Então se queremos uma Matriz do numero exato fazemos o valor menos ele, pois o indice 0 conta
+        // Então se queremos uma matriz de 8 fazemos j menos 8
         for (int j = 0; j < BITS_IN_BYTE; j++)
         {
-        // aqui fazemos a conta do código binário para converter o número em código binário, o bit é o resto da divisão do decimal
-        // divido por 2, depois em seguida fazer o decimal divido por 2 repetidas vezes, até seu numero ser igual a 8.
-        int bit = decimal % 2;
-        decimal /= 2;
-        // Bit é basicamente o digito do local em questão, para vermos quais luzes vai ligar no painel, é o número do indice em questão
-        print_bulb(bit);
+            // O Bloco de código em questão vai se repetir 8 vezes
+            // Aqui é a conta nos sempre queremos o resto da divisão por 2
+            // Pois nela só é possível dois resultados, 1 ou 0, pois ela normalmente representa Bar e Impar[
+            // E isso combina bastante com binários pois precisamos de 0 e 1.
+            bits[j] = decimal % 2;
+            // Aqui depois para acharmos o segundo digito fazemos isso dividindo o número por 2 repetidas vezes
+            // Até que chegue no total de 8 digitos
+            decimal /= 2;
         }
+
+        //Aqui a lógica é semelhante que a anterior, aqui queremos imprimir da forma certo os números binários
+        //Começamos da maneira inversa, fazemos que o 8 - 1 que da 7 digitos, o que é 8 digitos se contarmos do 0.
+        // Então utilizamos nossa variável com numero 8 mesno 1 para percorremos todos os digitos
+        //Definindo L como ela e fazemos o L repetir o loop se for igual ou menor que 0
+        // Já que o último digito que queremos é o 0
+        for(int l = BITS_IN_BYTE - 1; l >= 0; l--)
+        {
+            // Aqui por fim imprimos digitos, dos bits que fizemos no loop anterior com a variável 0.
+           print_bulb(bits[l]);
+        }
+
         //Aqui é só pra pular a linha quando mudar o caracter da setença
         printf("\n");
 
